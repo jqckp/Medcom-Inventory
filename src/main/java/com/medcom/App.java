8238cc5c626fc82cef51be1f9df94fe24047b7f8
Jack <porterjp1@appstate.edu>
2024-01-13 13:15:27 -0500

    Initialize maven project

---
+++ src/main/java/com/medcom/App.java
@@ -0,0 +1,13 @@
+package com.medcom;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
